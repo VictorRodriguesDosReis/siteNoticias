6b36383c424947cfa2907f3eaa829c9e7b332152
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2019-03-19 17:19:28 -0300

    Criado a função selectPrincipaisComImagem; Adicionado atributos que guardam as chamadas das procedures p_S_PrincipaisNoticiasDia, p_S_PrincipaisNoticiasSemana, p_S_PrincipaisNoticiasMes; Adicionado parâmetro quantidade as funções selectPrincipaisNoticiasDia, selectPrincipaisNoticiasSemana, selectPrincipaisNoticiasMes.

--- application/models/ModelHome.php
+++ application/models/ModelHome.php
@@ -5,9 +5,10 @@ class ModelHome extends CI_Model {
 	
 	private $noticiaParcial = 'CALL  p_S_NoticiaParcial(?)';
 	private $noticiaParcialFiltrado = 'CALL  p_S_NoticiaParcialFiltrado(?,?)';
-	private $principaisDia = 'CALL  p_S_PrincipaisNoticiasDia()';
-	private $principaisSemana = 'CALL  p_S_PrincipaisNoticiasSemana()';
-	private $principaisMes = 'CALL  p_S_PrincipaisNoticiasMes()';
+	private $principaisDia = 'CALL  p_S_PrincipaisNoticiasDia(?)';
+	private $principaisSemana = 'CALL  p_S_PrincipaisNoticiasSemana(?)';
+	private $principaisMes = 'CALL  p_S_PrincipaisNoticiasMes(?)';
+	private $principaisComImagem = 'CALL  p_S_PrincipaisComImagem(?)';
 
 	function __construct() {
 		parent::__construct();
@@ -38,24 +39,48 @@ class ModelHome extends CI_Model {
 		return $resultado;
 	}
 
-	public function selectPrincipaisNoticiasDia() {
-		$query = $this->db->query($this->principaisDia);
+	public function selectPrincipaisNoticiasDia($quantidade) {
+		$dados = array(
+			'quantidade' => $quantidade
+		);
+
+		$query = $this->db->query($this->principaisDia, $quantidade);
 		$resultado = $query->result_array();
 		$query->next_result();
   		$query->free_result();
 		return $resultado;
 	}
 
-	public function selectPrincipaisNoticiasSemana() {
-		$query = $this->db->query($this->principaisSemana);
+	public function selectPrincipaisNoticiasSemana($quantidade) {
+		$dados = array(
+			'quantidade' => $quantidade
+		);
+
+		$query = $this->db->query($this->principaisSemana, $quantidade);
 		$resultado = $query->result_array();
 		$query->next_result();
   		$query->free_result();
 		return $resultado;
 	}
 
-	public function selectPrincipaisNoticiasMes() {
-		$query = $this->db->query($this->principaisMes);
+	public function selectPrincipaisNoticiasMes($quantidade) {
+		$dados = array(
+			'quantidade' => $quantidade
+		);
+
+		$query = $this->db->query($this->principaisMes, $quantidade);
+		$resultado = $query->result_array();
+		$query->next_result();
+  		$query->free_result();
+		return $resultado;
+	}
+
+	public function selectPrincipaisComImagem($quantidade) {
+		$dados = array(
+			'quantidade' => $quantidade
+		);
+
+		$query = $this->db->query($this->principaisComImagem, $quantidade);
 		$resultado = $query->result_array();
 		$query->next_result();
   		$query->free_result();
