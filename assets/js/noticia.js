384319bea046ad919ff161a607c9382f68b440cf
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2019-03-19 16:25:37 -0300

    Adicionado a apresentação do comentário assim que ele é inserido no banco.

--- assets/js/noticia.js
+++ assets/js/noticia.js
@@ -7,8 +7,22 @@ $(document).ready(function() {
 		$.post(baseURL+'noticia/publicar-comentario', dados, function(resultado) {
 			if(resultado == 'error')
 				alert('Ocorreu um erro.');
-			else if (resultado == 'success')
-				alert('Comentário cadastrado !');
+			else {
+				let templateComentario = $('script[data-template="comentario"]').html();
+				let quantidadeComentarios = $('#div-comentarios .comentarios').length;
+				resultado = JSON.parse(resultado);
+
+				$('#nome').val('');
+				$('#comentario').val('');
+
+				templateComentario = templateComentario.replace(/{{nomeLeitor}}/g, resultado.nomeLeitor);
+				templateComentario = templateComentario.replace(/{{dataCriacao}}/g, resultado.dataCriacao);
+				templateComentario = templateComentario.replace(/{{comentario}}/g, resultado.comentario);
+
+				$('#div-comentarios .todos-comentarios').prepend(templateComentario);
+				$('#div-comentarios h2').text(++quantidadeComentarios +' Comentários');
+
+			}
 		});
 	});
 
