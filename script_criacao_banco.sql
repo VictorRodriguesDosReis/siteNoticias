d00b3506ef496f357065bec157afd5bbe759892c
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2019-03-19 16:14:22 -0300

    Criado nova tabela nomeada "tb_imagem" onde é guardado as imagens de capa de cada notícia; Criado as procedures p_I_Imagem, p_U_Noticia, p_U_DestativaNoticia, p_S_NoticiaParcialAutor, p_S_NoticiaParcialFiltradoAutor, p_S_NoticiaCompletaAutor, p_S_PrincipaisComImagem e p_S_NoticiaRecenteComImagem; Quando a procedure p_I_Noticia é chamada além de inserir a notícia ela retorna o código da notícia inserida; Quando a procedure p_I_Comentario é chamada além de inserir agora retorna dados do comentário inserido; As procedures p_S_NoticiaParcialFiltrado, p_S_NoticiaParcial, p_S_NoticiaCompleta, p_S_Comentarios foram modificadas para apenas retornar os dados solicitados quando o ic_ativo da noticias for igual a 1 (isso significa que a notícia não foi excluida pelo autor); As procedures p_S_PrinpaisNoticiasDia, p_S_PrincipaisNoticiasSemana e p_S_PrincipaisNoticiasMes foram modificadas para aceitarem parâmetro de quantidade de resultados.

--- script_criacao_banco.sql
+++ script_criacao_banco.sql
@@ -29,6 +29,17 @@ create table tb_noticia (
 			references tb_autor(cd_autor)
 );
 
+create table tb_imagem (
+	cd_imagem mediumint(8) not null auto_increment,
+    cd_noticia int(5) not null,
+    ds_caminho varchar(100) not null,
+    constraint pk_imagem
+		primary key (cd_imagem),
+	constraint fk_imagem_noticia
+		foreign key (cd_noticia)
+			references tb_noticia(cd_noticia)
+);
+
 create table tb_comentario (
 	cd_comentario mediumint(8) not null auto_increment,
     nm_leitor varchar(20) not null,
@@ -87,6 +98,14 @@ create procedure p_I_Noticia(titulo varchar(100), subtitulo varchar(250),
 begin
 	insert into tb_noticia (ds_titulo, ds_subtitulo, ds_noticia, dt_criacao, cd_autor) 
 		values (titulo, subtitulo, noticia, now(), codigoUsuario);
+	select last_insert_id() as codigo;
+end $$
+
+-- Insere as imagens que contém na notícia
+create procedure p_I_Imagem(caminho varchar(100), codigoNoticia int(5))
+begin
+	insert into tb_imagem(ds_caminho, cd_noticia) 
+		values (caminho, codigoNoticia);
 end $$
 
 -- Insere um novo comentário no banco
@@ -94,6 +113,10 @@ create procedure p_I_Comentario (nome varchar(20), comentario varchar(255), codi
 begin
 	insert into tb_comentario(nm_leitor, ds_comentario, cd_noticia)
 		values (nome, comentario, codigoNoticia);
+        
+	select nm_leitor nomeLeitor, dt_criacao dataCriacao, ds_comentario comentario
+		from tb_comentario
+			where cd_comentario = last_insert_id();
 end $$
 
 
@@ -135,6 +158,28 @@ begin
     
 end $$
 
+-- Atualiza as informações da notícia
+create procedure p_U_Noticia(titulo varchar(100), subtitulo varchar(250), 
+	noticia longtext, codigoUsuario int(2), codigoNoticia int(5))
+begin
+	update tb_noticia 
+		set ds_titulo = titulo, ds_subtitulo = subtitulo,
+			ds_noticia = noticia
+				where cd_autor = codigoUsuario and cd_noticia = codigoNoticia;
+                
+	select ds_titulo titulo, ds_subtitulo subtitulo, dt_alteracao dataEdicao
+		from tb_noticia 
+			where cd_autor = codigoUsuario and cd_noticia = codigoNoticia;
+    
+end $$	
+
+-- Desativa a noticia impossibilitando de ser apresentada no site
+create procedure p_U_DesativaNoticia(codigoUsuario int(2), codigoNoticia int(5))
+begin
+	update tb_noticia set ic_ativo = 0
+		where cd_autor = codigoUsuario and cd_noticia = codigoNoticia;
+end $$
+
 
 -- SELECT
 -- Verifica se existe o usuario no banco
@@ -148,7 +193,8 @@ create procedure p_S_NoticiaParcialFiltrado(codigoInicio int(5), quantidade int(
 begin
 	select cd_noticia codigo, ds_titulo titulo, ds_subtitulo subtitulo, dt_alteracao data
 		from tb_noticia 
-			where cd_noticia < codigoInicio order by dt_alteracao desc limit quantidade;
+			where cd_noticia < codigoInicio and ic_ativo = 1
+				order by dt_alteracao desc limit quantidade;
 end $$
 
 -- Retorna a quantidade solicitada de notícias mais recentes
@@ -156,7 +202,26 @@ create procedure p_S_NoticiaParcial(quantidade int(5))
 begin
 	select cd_noticia codigo, ds_titulo titulo, ds_subtitulo subtitulo, dt_alteracao data
 		from tb_noticia 
-			where cd_noticia order by dt_alteracao desc limit quantidade;
+			where ic_ativo = 1
+				order by dt_alteracao desc limit quantidade;
+end $$
+
+-- Retorna a quantidade solicitada de notícias mais recentes do autor
+create procedure p_S_NoticiaParcialAutor(quantidade int(5), codigo int(2))
+begin
+	select cd_noticia codigo, ds_titulo titulo, ds_subtitulo subtitulo, dt_alteracao data
+		from tb_noticia
+			where cd_autor = codigo and ic_ativo = 1
+				order by dt_alteracao desc limit quantidade;
+end $$
+
+-- Retorna uma determinada quantidade de notícia do autor, iniciando pelo ponto desejado
+create procedure p_S_NoticiaParcialFiltradoAutor(codigoInicio int(5), quantidade int(5), codigo int(2))
+begin
+	select cd_noticia codigo, ds_titulo titulo, ds_subtitulo subtitulo, dt_alteracao data
+		from tb_noticia 
+			where cd_noticia < codigoInicio and cd_autor = codigo and ic_ativo = 1
+				order by dt_alteracao desc limit quantidade;
 end $$
 
 -- Retorna informações da notícia solicitada
@@ -165,7 +230,15 @@ begin
 	select ds_titulo titulo, ds_subtitulo subtitulo, ds_noticia noticia, 
 		(select nm_autor from tb_autor a where a.cd_autor = n.cd_autor) autor, dt_criacao, dt_alteracao
 		from tb_noticia n
-			where cd_noticia = codigo;
+			where cd_noticia = codigo and ic_ativo = 1;
+end $$
+
+-- Retorna informações da notícia solicitada pelo autor
+create procedure p_S_NoticiaCompletaAutor(codigoNoticia int(5), codigoAutor int(2))
+begin
+	select cd_noticia codigo, ds_titulo titulo, ds_subtitulo subtitulo, ds_noticia noticia
+		from tb_noticia 
+			where cd_autor = codigoAutor and cd_noticia = codigoNoticia and ic_ativo = 1;
 end $$
 
 -- Retorna os comentários da notícia solicitada
@@ -173,34 +246,63 @@ create procedure p_S_Comentarios(codigo int(5))
 begin
 	select nm_leitor leitor, ds_comentario comentario, dt_criacao
 		from tb_comentario 
-			where cd_noticia = codigo;
+			where cd_noticia = codigo and ic_ativo = 1
+				order by cd_comentario desc;
 end $$
 
 -- Seleciona as 5 notícias mais visualizadas do dia
-create procedure p_S_PrincipaisNoticiasDia()
+create procedure p_S_PrincipaisNoticiasDia(quantidade tinyint(1))
 begin
 	select n.ds_titulo titulo, n.ds_subtitulo subtitulo, n.cd_noticia codigo
 		from tb_visualizacao_dia vd
 			inner join tb_noticia n on vd.cd_noticia = n.cd_noticia
-				order by vd.qt_visualizacao desc limit 5; 
+				where ic_ativo = 1
+					order by vd.qt_visualizacao desc limit quantidade; 
 end $$
 
 -- Seleciona as 5 notícias mais visualizadas da semana
-create procedure p_S_PrincipaisNoticiasSemana()
+create procedure p_S_PrincipaisNoticiasSemana(quantidade tinyint(1))
 begin
 	select n.ds_titulo titulo, n.ds_subtitulo subtitulo, n.cd_noticia codigo
 		from tb_visualizacao_semana vs
 			inner join tb_noticia n on vs.cd_noticia = n.cd_noticia
-				order by vs.qt_visualizacao desc limit 5; 
+				where ic_ativo = 1
+					order by vs.qt_visualizacao desc limit quantidade; 
 end $$
 
 -- Seleciona as 5 notícias mais visualizadas do mês
-create procedure p_S_PrincipaisNoticiasMes()
+create procedure p_S_PrincipaisNoticiasMes(quantidade tinyint(1))
 begin
 	select n.ds_titulo titulo, n.ds_subtitulo subtitulo, n.cd_noticia codigo
 		from tb_visualizacao_mes vm
 			inner join tb_noticia n on vm.cd_noticia = n.cd_noticia
-				order by vm.qt_visualizacao desc limit 5; 
+				where ic_ativo = 1
+					order by vm.qt_visualizacao desc limit quantidade; 
+end $$
+
+-- Seleciona uma quantidade determinada de principais notícias da semana com imagem
+create procedure p_S_PrincipaisComImagem(quantidade tinyint(1))
+begin
+	select n.cd_noticia codigo, n.ds_titulo titulo, n.ds_subtitulo subtitulo, i.ds_caminho imagem
+	from tb_noticia n, tb_imagem i, tb_visualizacao_semana vs
+		where n.cd_noticia = vs.cd_noticia and
+			  n.cd_noticia = i.cd_noticia and 
+              n.ic_ativo = 1
+				group by vs.cd_noticia
+                order by vs.qt_visualizacao desc
+                limit quantidade;
+end $$
+
+-- Seleciona um quantidade determinada das notícias recentes
+create procedure p_S_NoticiaRecenteComImagem(quantidade tinyint(1))
+begin
+	select n.cd_noticia codigo, n.ds_titulo titulo, i.ds_caminho imagem
+		from tb_noticia n, tb_imagem i
+			where n.cd_noticia = i.cd_noticia and
+				  n.ic_ativo = 1
+					group by n.cd_noticia
+					order by n.cd_noticia desc
+					limit quantidade;
 end $$
 
 delimiter ;
